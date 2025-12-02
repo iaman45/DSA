64137eeaa6e21473fcf182420d31d2217ede7325
Aman Kumar <iaman030945@gmail.com>
2025-12-02 14:40:03 +0530

    Time: 1 ms (99.89%) | Memory: 42.9 MB (38.66%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] freq = new int[128];
+
+        for (char c : s.toCharArray()) {
+            freq[c]++;
+        }
+
+        int ans = 0;
+        boolean hasOdd = false;
+
+        for (int f : freq) {
+            if (f % 2 == 0) {
+                ans += f;
+            } else {
+                ans += (f - 1);
+                hasOdd = true;
+            }
+        }
+
+        return hasOdd ? ans + 1 : ans;
+    }
+}
